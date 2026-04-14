767353074043d8c0dcae3235afee424333c2b77b
vagish <vagish.sachdev@atomise.com>
2026-04-14 15:05:15 +0530

    Widgets: Padding, Button, TextField, DebugModes,  DebugPaintSize

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MainApp());
+  debugPaintSizeEnabled = false;
 }
 
 class MainApp extends StatelessWidget {
@@ -9,6 +12,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.black,
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(5),
+    );
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
@@ -16,42 +28,62 @@ class MainApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              
+              // == TEXT ==
               const Text(
                 '0',
                 style: TextStyle(
                   fontSize: 55,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black,
+                  color: Colors.white,
                 ),
               ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Enter amount in USD',
-                  hintStyle: const TextStyle(color: Colors.black),
-                  prefixIcon: Icon(Icons.monetization_on),
-                  prefixIconColor: Colors.black,
-                  filled: true,
-                  fillColor: Colors.white,
-                  // when TextField is in focus
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: Colors.black,
-                      width: 2.0,
-                      style: BorderStyle.solid,
-                    ),
-                    borderRadius: BorderRadius.circular(5),
+
+              // == TEXT FIELD ==
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter amount in USD',
+                    hintStyle: const TextStyle(color: Colors.black),
+                    prefixIcon: Icon(Icons.monetization_on),
+                    prefixIconColor: Colors.black,
+                    filled: true,
+                    fillColor: Colors.white,
+                    // when TextField is in focus
+                    focusedBorder: border,
+                    // when TextField is idle 
+                    enabledBorder: border,
                   ),
-                  // when TextField is idle
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: Colors.black,
-                      width: 2.0,
-                      style: BorderStyle.solid,
-                    ),
-                    borderRadius: BorderRadius.circular(5),
+                  // limiting input to number
+                  keyboardType: TextInputType.numberWithOptions(
+                    decimal: true,
+                    signed: false,
                   ),
                 ),
               ),
+
+              // == BUTTON ==
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (kDebugMode) print('clicked');
+                  }, 
+                  style: const ButtonStyle(
+                    elevation: WidgetStatePropertyAll(15),
+                    backgroundColor: WidgetStatePropertyAll(Colors.black),
+                    foregroundColor: WidgetStatePropertyAll(Colors.white),
+                    minimumSize: WidgetStatePropertyAll(
+                      Size(double.infinity,50)
+                    ),
+                  ),
+                  child: const Text(
+                    'Convert',
+                  ),
+                ),
+              ),
+
             ],
           ),
         ),
