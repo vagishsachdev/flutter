157fbd1817475acf01f7babfe08c49122cc8ebd3
vagish <vagish.sachdev@atomise.com>
2026-04-14 11:28:41 +0530

    Widget alignment practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Row(
+            spacing: 20,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ColumnBar(),
+              ColumnBar(),
+              ColumnBar()
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class ColumnBar extends StatelessWidget {
+  const ColumnBar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const ColoredBox(
+      color: Colors.blue,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text('Test')],
       ),
     );
   }
